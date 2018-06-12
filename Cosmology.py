83b421761bcc2cf7b504b11eba95a6301e59ce65
Boehm, Vanessa Martina (vboehm) <boehmvanessa@gmail.com>
2018-06-11 17:19:25 -0700

    more curves sent to Jia and Will and retested

--- Cosmology.py
+++ Cosmology.py
@@ -77,11 +77,11 @@ JiaNu={
 'N_ncdm' : 3,
 'm_ncdm' : str(0.02188)+','+str(0.02352)+','+str(0.05519),
 'P_k_max_1/Mpc': 10.,
-'ncdm_fluid_approximation': 3,
+'ncdm_fluid_approximation': 1,
 'deg_ncdm' : '1.0, 1.0, 1.0',
-'l_max_ncdm' : 100,
-'tol_ncdm_bg':1e-10,
-'tol_ncdm_synchronous':1.e-5,
+#'l_max_ncdm' : 100,
+#'tol_ncdm_bg':1e-10,
+#'tol_ncdm_synchronous':1.e-5,
 }
 
 # getting 9965 difference with this
